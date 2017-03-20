1cbe05ec4d67ae587cd77d0a1a130e1a9a187271
Steve Gordon <steve.gordon@madgex.com>
2017-03-20 13:15:03 +0000

    Initial Commit

---
+++ src/WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace WebApplication5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Joe Bloggs", "jbloggs@example.com"));
+            emailMessage.To.Add(new MailboxAddress("", "jbloggs@example.com"));
+            emailMessage.Subject = "test";
+            emailMessage.Body = new TextPart("plain") { Text = "test" };
+
+            using (var client = new SmtpClient())
+            {
+                client.LocalDomain = "some.domain.com";
+                await client.ConnectAsync("localhost", 25, SecureSocketOptions.None).ConfigureAwait(false);
+                await client.SendAsync(emailMessage).ConfigureAwait(false);
+                await client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
